66e766277741e090e9e3490abe23ec09d1c0b2e5
Alexey <alexey.korchuganov@flant.com>
2025-07-04 19:32:13 +0700

    fix: simplify initialization in main.ts by removing async function and directly calling initAuth

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import App from './app/App.vue'
 import { router } from './app/router'
 import { initAuth } from './shared/api/supabase/auth'
 
-async function bootstrap() {
-  await initAuth()
-  createApp(App).use(router).mount('#app')
-}
+initAuth()
 
-bootstrap()
+createApp(App).use(router).mount('#app')
