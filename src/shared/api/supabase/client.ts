4532c183aff4e0d2d779ca1bceb007958f4adbcc
Alexey <alexey.korchuganov@flant.com>
2025-07-06 03:40:04 +0700

    fix: remove CustomSupabaseClient class and streamline Supabase client initialization

--- src/shared/api/supabase/client.ts
+++ src/shared/api/supabase/client.ts
@@ -1,14 +1,7 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-
-class CustomSupabaseClient extends SupabaseClient {
-  setToken(newToken: string) {
-    this.rest.headers['Authorization'] = `Bearer ${newToken}`
-  }
-}
 
 let supabase = createClient(supabaseUrl, supabaseAnonKey)
 
